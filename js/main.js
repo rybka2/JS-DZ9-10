70bace75d4563b77ca1b1794129d0bd77b223ad4
rybka2 <annmychailova@gmail.com>
2016-11-21 12:10:49 +0200

    commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+ $(document).ready(function() {
+     $( '.dropdown' ).hover(
+         function(){
+             $(this).children('.sub-menu').slideDown(200);
+         },
+         function(){
+             $(this).children('.sub-menu').slideUp(200);
+         }
+     );
+ });
+ //carousel
+(function($) {
+    $(function() {
+        $('.jcarousel').jcarousel();
+
+        $('.jcarousel-control-prev')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.jcarousel-control-next')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '+=1'
+            });
+
+        $('.jcarousel-pagination')
+            .on('jcarouselpagination:active', 'a', function() {
+                $(this).addClass('active');
+            })
+            .on('jcarouselpagination:inactive', 'a', function() {
+                $(this).removeClass('active');
+            })
+            .jcarouselPagination();
+    });
+})(jQuery);
+
+//select
+$(document).ready(function() {
+    $("select").searchable();
+});
